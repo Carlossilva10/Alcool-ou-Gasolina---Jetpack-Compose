909662c37abe4dca276705e82bf2e57252fc35a8
CarlosSilva <caduaba@hotmail.com>
2025-04-09 01:39:29 -0300

    andamento do projeto, colocando cor espaços entre os textos.

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
@@ -10,19 +10,23 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.alcoolougasolina.ui.theme.AlcoolOuGasolinaTheme
 
@@ -52,16 +56,46 @@ fun App(){
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Álcool ou Gasolina",
-            style = TextStyle(
-                color = Color.White,
-                fontSize = 32.sp,
-                fontWeight = FontWeight.Bold
+        Column (
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+
+            Text(
+                text = "Álcool ou Gasolina",
+                style = TextStyle(
+                    color = Color.White,
+                    fontSize = 32.sp,
+                    fontWeight = FontWeight.Bold
+                )
             )
-        )
-        Text(text = "Gasolina")
-        TextField(value = "2.00", onValueChange = {})
-        TextField(value = "2.00", onValueChange = {})
+
+            Text(
+                text = "Gasolina",
+                style = TextStyle(
+                    color = Color.Red,
+                    fontSize = 40.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+
+            var valorGasolina = mutableStateOf("")
+
+            TextField(
+                value = "2.00",
+                onValueChange = {},
+                label = {
+                    Text(text = "Gasolina")
+                }
+            )
+            TextField(
+                value = "2.00",
+                onValueChange = {},
+                label = {
+                    Text(text = "Álcool")
+                }
+            )
+        }
     }
 }
 
